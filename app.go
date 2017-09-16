a4e8a7dbe4df92e8e41f73f3c97795b3c7ffcd38
nelsonken <gengkang@live.com>
2017-09-16 10:29:43 +0800

    add middleware group register method and it's test

--- app.go
+++ app.go
@@ -8,9 +8,13 @@ import (
 type App struct {
 	*echo.Echo
 
-	Logger   *Logger
-	Services *Container
+	Logger          *Logger
+	Services        *Container
+	MiddlewareGroup MiddlewareGroup
 }
+
+// MiddlewareGroup string key is the route prefix, value is []MiddlewareFunc
+type MiddlewareGroup = map[string][]MiddlewareFunc
 
 // AppContext is wrapper of echo.Context. It holds App instance of server.
 type AppContext struct {
@@ -41,4 +45,29 @@ func AppFromContext(ctx Context) *App {
 		return nil
 	}
 	return ac.App()
+}
+
+// GetGroup get a middlware group
+func (app *App) GetRouteGroup(prefix string) *echo.Group {
+	g := app.Echo.Group(prefix)
+	if app.MiddlewareGroup == nil {
+		return g
+	}
+
+	middlewares, ok := app.MiddlewareGroup[prefix]
+	if !ok {
+		return g
+	}
+
+	g.Use(middlewares...)
+
+	return g
+}
+
+func (app *App) AddMiddlewareGroup(prefix string, m []MiddlewareFunc) {
+	if app.MiddlewareGroup == nil {
+		app.MiddlewareGroup = MiddlewareGroup{}
+	}
+
+	app.MiddlewareGroup[prefix] = m
 }
